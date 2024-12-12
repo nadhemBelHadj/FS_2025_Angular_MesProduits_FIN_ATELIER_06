1bac7e820270e41ab88d9556fb522fd253d24af1
unknown <nadhemb@yahoo.com>
2024-12-12 11:31:39 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ProduitsComponent } from './produits/produits.component';
+import { AddProduitComponent } from './add-produit/add-produit.component';
+import { UpdateProduitComponent } from './update-produit/update-produit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "produits", component : ProduitsComponent},
+    {path: "add-produit", component : AddProduitComponent},
+    {path: "updateProduit/:id",  component: UpdateProduitComponent},
+    {path: "", redirectTo: "produits", pathMatch: "full"}
+];
